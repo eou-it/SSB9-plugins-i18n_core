fc91e905f0e238201c4c4775acaed078b6b4aae4
unknown <jayachandrak@.corp.sct.com>
2012-03-13 21:21:22 +0530

    Added maven

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,49 +9,34 @@
  Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
  Education in the U.S.A. and/or other regions and/or countries.
  **********************************************************************************/
- 
- 
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
-
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
-    
     // inherit Grails' default dependencies
-    inherits( "global" ) {
+    inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
     }
-    
-    
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-
-    distribution = {
-         localRepository = ""
-         remoteRepository( id:"snapshots", url:"http://m038083.sungardhe.com:8081/nexus/content/repositories/snapshots" ) {
-              authentication  username:'admin', password:'admin123'
-         }
-         remoteRepository( id:"releases", url:"http://m038083.sungardhe.com:8081/nexus/content/repositories/releases" ) {
-             authentication  username:'admin', password:'admin123'
-         }
-    }  
-      
-    
     repositories {
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/releases/"
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/snapshots/"
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/thirdparty/"
-        
         grailsPlugins()
         grailsHome()
         grailsCentral()
-        
-        mavenCentral()
-        mavenRepo "http://repository.jboss.org/maven2/"
-        mavenRepo "http://repository.codehaus.org"
-    }
 
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        //mavenCentral()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
     dependencies {
-    }
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
+        // runtime 'mysql:mysql-connector-java:5.1.5'
+    }
 }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -9,48 +9,29 @@
  Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
  Education in the U.S.A. and/or other regions and/or countries.
  **********************************************************************************/
+// configuration for plugin testing - will not be included in the plugin zip
+ 
+log4j = {
+    // Example of changing the log pattern for the default console
+    // appender:
+    //
+    //appenders {
+    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+    //}
 
+    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+           'org.codehaus.groovy.grails.web.pages', //  GSP
+           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+           'org.codehaus.groovy.grails.web.mapping', // URL mapping
+           'org.codehaus.groovy.grails.commons', // core / classloading
+           'org.codehaus.groovy.grails.plugins', // plugins
+           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+           'org.springframework',
+           'org.hibernate',
+           'net.sf.ehcache.hibernate'
 
-// Note: Since there is a separate test app (see test-banner-core under tests), this Config.groovy is 
-// only used for tailoring the documentation generated via 'grails Doc'. 
-
-grails.doc.authors = '''Prepared by: SunGard Higher Education
-                        |4 Country View Road Malvern, Pennsylvania 19355 United States of America (800) 522 - 4827'''.stripMargin()
-                        
-grails.doc.footer = '''Contains confidential and proprietary information of SunGard and its subsidiaries.'''
-
-grails.doc.license = '''Use of these materials is limited to SunGard Higher Education licensees, and is subject to the terms and conditions of one or more written license agreements between SunGard Higher Education and the licensee in question.
-                        |In preparing and providing this publication, SunGard Higher Education is not rendering legal, accounting, or other similar professional services. SunGard Higher Education makes no claims that an institution's use of this publication or the software for which it is provided will insure compliance with applicable federal or state laws, rules, or regulations. Each organization should seek legal, accounting and other similar professional services from competent providers of the organization’s own choosing.
-                        |'''.stripMargin()
-
-grails.doc.copyright = '''© 2010-2011 SunGard. All rights reserved.
-                          |Use of these materials is limited to SunGard Higher Education licensees, and is subject to the terms and conditions of one or more written license agreements between SunGard Higher Education and the licensee in question.
-                          |In preparing and providing this publication, SunGard Higher Education is not rendering legal, accounting, or other similar professional services. SunGard Higher Education makes no claims that an institution's use of this publication or the software for which it is provided will insure compliance with applicable federal or state laws, rules, or regulations. Each organization should seek legal, accounting and other similar professional services from competent providers of the organization’s own choosing.
-                          |'''.stripMargin()
-
-grails.doc.images = new File( "src/docs/images" )
-grails.doc.logo = '''<img src="../img/main-banner-image.png">'''
-grails.doc.sponsorLogo = '''<img src="../img/sghe_global.logo.jpg">'''
-
-grails.doc.alias.release_notes = "1. Release Notes"
-grails.doc.alias.user          = "2. User Guide"
-grails.doc.alias.overview      = "2.1 Architecture Overview"
-grails.doc.alias.security      = "2.2 Application Security"
-grails.doc.alias.services      = "2.3 Services"
-grails.doc.alias.rest          = "2.4 REST"
-grails.doc.alias.dev           = "3. Developer Guide"
-
-
-// CodeNarc rulesets
-codenarc.ruleSetFiles="rulesets/banner.groovy"
-codenarc.reportName="target/CodeNarcReport.html"
-codenarc.propertiesFile="grails-app/conf/codenarc.properties"
-
-
-// Code Coverage configuration
-coverage {
-	enabledByDefault = false
+    warn   'org.mortbay.log'
 }
-
 grails.views.default.codec="none" // none, html, base64
 grails.views.gsp.encoding="UTF-8"
