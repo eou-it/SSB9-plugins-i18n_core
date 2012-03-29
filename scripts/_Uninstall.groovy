7289cfa3b26b91757f05f4d128a39794f1519d4e
Jay Kadiam <jayachandra.kadiam@sungardhe.com>
2012-03-29 22:28:20 +0530

    Added scripts for zk

--- scripts/_Uninstall.groovy
+++ scripts/_Uninstall.groovy
@@ -3,3 +3,5 @@
 // Use this script if you intend to do any additional clean-up on uninstall, but
 // beware of messing up SVN directories!
 //
+
+ant.delete(file:"${basedir}/web-app/WEB-INF/i18n-core-lang-addon.xml")
--- scripts/_Upgrade.groovy
+++ scripts/_Upgrade.groovy
@@ -9,4 +9,21 @@
 //    ant.mkdir(dir:"${basedir}/grails-app/jobs")
 //
 
-ant.move(file:"${pluginBasedir}/src/java/metainfo/zk/lang-addon.xml", tofile:"${basedir}/web-app/WEB-INF/i18n-core-lang-addon.xml", overwrite:"true")
+ant.move(file: "${pluginBasedir}/src/java/metainfo/zk/lang-addon.xml", tofile: "${basedir}/web-app/WEB-INF/i18n-core-lang-addon.xml", overwrite: "true")
+
+def zkFile = "${basedir}/web-app/WEB-INF/zk.xml"
+def targetLocation = "${pluginBasedir}/src/java/web"
+
+if ((new File(zkFile).exists())) {
+
+    if ((new File(targetLocation).exists())) {
+        ant.delete(dir: targetLocation)
+    }
+
+    ant.move(todir: targetLocation + "/js") {
+        fileset(dir: "${pluginBasedir}/web-app/js")
+    }
+    ant.move(todir: targetLocation + "/css") {
+        fileset(dir: "${pluginBasedir}/web-app/css")
+    }
+}
