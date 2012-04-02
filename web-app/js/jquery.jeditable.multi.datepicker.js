a1c41b5f8de1556af8893f01c239eb63ec65e095
Navein Austin Fernandes <Navein.Fernandes@sungardhe.com>
2012-04-02 16:16:16 +0530

    Modified jeditable to remove all initialization params for multi calendar.

--- web-app/js/jquery.jeditable.multi.datepicker.js
+++ web-app/js/jquery.jeditable.multi.datepicker.js
@@ -1,86 +1,3 @@
-/*
- * Datepicker for Jeditable
- *
- * Copyright (c) 2011 Piotr 'Qertoip' Włodarek
- *
- * Licensed under the MIT license:
- *   http://www.opensource.org/licenses/mit-license.php
- *
- * Depends on jQuery UI Datepicker
- *
- * Project home:
- *   http://github.com/qertoip/jeditable-datepicker
- *
- */
-
-$.calendars.picker.setDefaults({
-    renderer:          $.calendars.picker.themeRollerRenderer,
-    changeMonth:       false,
-    showAnim:          "fadeIn",
-    showOptions:       null,
-    showSpeed:         500,
-    useMouseWheel:     false,
-    showOtherMonths:   true,
-    selectOtherMonths: true,
-    changeMonth:       false,
-    prevText:          "<span class=\"ui-icon ui-icon-circle-triangle-w\"> </span>",
-    nextText:          "<span class=\"ui-icon ui-icon-circle-triangle-e\"> </span>"
-});
-
-var default_calendar='islamic';
-var default_calendar1='islamic';
-var default_calendar2='gregorian';
-var default_calendar3='gregorian';
-
-//var default_calendar1='gregorian';
-//var default_calendar2='islamic';
-
-
-//var default_multicalendar_date_format='mm/dd/yyyy';
-
-//This is a mandate for widget. It needs can only accept dates that are in integer format.
-//var default_service_date_format='MM/dd/yyyy';
-
-var converters = {
-    gregorianToIslamic: {
-        format: {
-            url: 'dateConverter',
-            nameOfDateParam: 'date',
-            extraParams: {
-                calendar: 'islamic-civil',
-                fromDateFormat: $.multicalendar.JAVA_DATE_FORMAT,
-                toDateFormat: $.multicalendar.JAVA_DATE_FORMAT,
-                toULocale: 'en_US@calendar=islamic-civil',
-                fromULocale: 'en_US@calendar=gregorian'
-            }
-        }
-    },
-
-    islamicToGregorian: {
-        format: {
-            url: 'dateConverter',
-            nameOfDateParam: 'date',
-            extraParams: {
-                calendar: 'islamic-civil',
-                fromDateFormat: $.multicalendar.JAVA_DATE_FORMAT,
-                toDateFormat: $.multicalendar.JAVA_DATE_FORMAT,
-                toULocale: 'en_US@calendar=gregorian',
-                fromULocale: 'en_US@calendar=islamic-civil'
-            }
-        }
-    }
-};
-
-var dateFormats = {islamic: 'dd/M/yyyy'};
-$.multicalendar.setDefaults({
-    defaultCalendar: default_calendar,
-    converters: converters,
-    dateFormats: dateFormats,
-    calendars:[ default_calendar1, default_calendar2 ],
-    firstDayOfTheWeek: 4,
-    isRTL: false
-});
-
 // add :focus selector
 jQuery.expr[':'].focus = function( elem ) {
   return elem === document.activeElement && ( elem.type || elem.href );
@@ -149,13 +66,6 @@ $.editable.addInputType( 'datepicker', {
         jQuery.extend(datepicker, settings.datepicker);
       }
 
-      //input.id = 'multiCalendarDestination';
-
-
-
-      //input.multiDatePicker([calendar1Options, calendar2Options])
       input.multiDatePicker();
-
-
     }
 } );
