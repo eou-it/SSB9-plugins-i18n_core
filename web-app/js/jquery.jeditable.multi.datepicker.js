6060889cbac17456d622f36a672f1514dd891129
Kevin J. Butler <kevin.butler@ellucian.com>
2013-06-10 09:15:13 -0600

    Initialize jeditable datepicker after underscore and jquery-editable are intialized (and remove local copy of _.wrap). This file should probably move back to banner_ui_ss, as it is only useful in self-service and requires functionality provided by banner_ui_ss.

--- web-app/js/jquery.jeditable.multi.datepicker.js
+++ web-app/js/jquery.jeditable.multi.datepicker.js
@@ -3,76 +3,70 @@ jQuery.expr[':'].focus = function( elem ) {
   return elem === document.activeElement && ( elem.type || elem.href );
 };
 
-/* _wrap copied from underscore.js to avoid circular dependency on banner_ui_ss */
-var _wrap =  function(func, wrapper) {
-    return function() {
-      var args = [func];
-      push.apply(args, arguments);
-      return wrapper.apply(this, args);
-    };
-  };
+$(function() {
+    // requires _ and jquery.editable to be initialized
+    $.datepicker._doKeyDown = _.wrap( $.datepicker._doKeyDown, function(func, event) {
+        if ( !this._pressedKeys && event.keyCode == 13 ) {
+            // if ENTER is the first keypress in the open datepicker, just close it
+            $.datepicker._hideDatepicker();
+        } else {
+            this._pressedKeys = true;
+            return func( event );
+        }
+    });
 
-$.datepicker._doKeyDown = _wrap( $.datepicker._doKeyDown, function(func, event) {
-    if ( !this._pressedKeys && event.keyCode == 13 ) {
-        // if ENTER is the first keypress in the open datepicker, just close it
-        $.datepicker._hideDatepicker();
-    } else {
-        this._pressedKeys = true;
-        return func( event );
-    }
-});
+    $.editable.addInputType( 'datepicker', {
 
-$.editable.addInputType( 'datepicker', {
-
-    /* create input element */
-    element: function( settings, original ) {
-      var form = $( this ),
-          input = $( '<input id="multiCalendarDestination"/>' );
-      input.attr( 'autocomplete','off' );
-      form.append( input );
-      return input;
-    },
-
-    /* attach jquery.ui.datepicker to the input element */
-    plugin: function( settings, original ) {
-      var form = this,
-          input = form.find( "input" );
-
-      // Don't cancel inline editing onblur to allow clicking datepicker
-      settings.onblur = 'nothing';
-
-      datepicker = {
-        onSelect: function() {
-          // clicking specific day in the calendar should
-          // submit the form and close the input field
-          form.submit();
+        /* create input element */
+        element: function( settings, original ) {
+            var form = $( this ),
+            input = $( '<input id="multiCalendarDestination"/>' );
+            input.attr( 'autocomplete','off' );
+            form.append( input );
+            return input;
         },
 
-        onClose: function() {
-          setTimeout( function() {
-            if ( !input.is( ':focus' ) ) {
-              // input has NO focus after 150ms which means
-              // calendar was closed due to click outside of it
-              // so let's close the input field without saving
-              original.reset( form );
-            } else {
-              // input still HAS focus after 150ms which means
-              // calendar was closed due to Enter in the input field
-              // so lets submit the form and close the input field
-              form.submit();
+        /* attach jquery.ui.datepicker to the input element */
+        plugin: function( settings, original ) {
+            var form = this,
+            input = form.find( "input" );
+
+            // Don't cancel inline editing onblur to allow clicking datepicker
+            settings.onblur = 'nothing';
+
+            datepicker = {
+                onSelect: function() {
+                    // clicking specific day in the calendar should
+                    // submit the form and close the input field
+                    form.submit();
+                },
+
+                onClose: function() {
+                    setTimeout( function() {
+                        if ( !input.is( ':focus' ) ) {
+                            // input has NO focus after 150ms which means
+                            // calendar was closed due to click outside of it
+                            // so let's close the input field without saving
+                            original.reset( form );
+                        } else {
+                            // input still HAS focus after 150ms which means
+                            // calendar was closed due to Enter in the input field
+                            // so lets submit the form and close the input field
+                            form.submit();
+                        }
+
+                        // the delay is necessary; calendar must be already
+                        // closed for the above :focus checking to work properly;
+                        // without a delay the form is submitted in all scenarios, which is wrong
+                    }, 150 );
+                }
+            };
+
+            if (settings.datepicker) {
+                jQuery.extend(datepicker, settings.datepicker);
             }
 
-            // the delay is necessary; calendar must be already
-            // closed for the above :focus checking to work properly;
-            // without a delay the form is submitted in all scenarios, which is wrong
-          }, 150 );
+            input.multiCalendarPicker(datepicker);
         }
-      };
-
-      if (settings.datepicker) {
-        jQuery.extend(datepicker, settings.datepicker);
-      }
-
-      input.multiCalendarPicker(datepicker);
-    }
-} );
+    } );
+});
