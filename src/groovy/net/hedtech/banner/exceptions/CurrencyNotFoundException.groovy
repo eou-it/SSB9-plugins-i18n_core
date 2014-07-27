47d5bc2017f9a64e2ddb3e7bc3b93bb32e57dca2
karthick <karthic.kumar@ellucian.com>
2014-07-27 11:56:16 +0530

    HRU-5341 - Moved MessageHelper class to i18_core plugin from sghe_zk_core. Also added error message for invalid currency code.

--- src/groovy/net/hedtech/banner/exceptions/CurrencyNotFoundException.groovy
+++ src/groovy/net/hedtech/banner/exceptions/CurrencyNotFoundException.groovy
@@ -2,17 +2,19 @@
 Copyright 2009-2014 Ellucian Company L.P. and its affiliates.
 *******************************************************************************/
 package net.hedtech.banner.exceptions
+
+import net.hedtech.banner.ui.zk.i18n.MessageHelper
+
 /**
  * A runtime exception indicating an entity or resource was not found.
  **/
 public class CurrencyNotFoundException extends RuntimeException {
 
-    public static final String MESSAGE_KEY_CURRCODE_INVALID = "currcode.invalid.message"
+    public static final String MESSAGE_KEY_CURRCODE_INVALID = "default.invalid.currcode"
     def currencyCode
 
     public String getMessage() {
-       //     MessageResolver.message(MESSAGE_KEY_CURRCODE_INVALID, [currencyCode]?.toArray() )
-        return MESSAGE_KEY_CURRCODE_INVALID
+        return MessageHelper.message(MESSAGE_KEY_CURRCODE_INVALID, [currencyCode]?.toArray() )
     }
 
     String toString() {
