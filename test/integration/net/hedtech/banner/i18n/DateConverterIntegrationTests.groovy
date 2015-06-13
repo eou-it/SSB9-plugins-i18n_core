6e9d06b62f56bf8f111cc886676a19f772d59963
RajanandPK <rajanandppk@ellucian.com>
2015-06-14 00:18:36 +0530

    No JIRA: the testcase covers Canada_French locale setting was not recognised in UNIX environment, so fails

--- test/integration/net/hedtech/banner/i18n/DateConverterIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/i18n/DateConverterIntegrationTests.groovy
@@ -5,9 +5,11 @@ package net.hedtech.banner.i18n
 
 import grails.converters.JSON
 import net.hedtech.banner.i18n.utils.LocaleUtilities
-import org.codehaus.groovy.grails.web.json.JSONArray
 import org.codehaus.groovy.grails.web.json.JSONObject
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest
+import org.junit.After
 import org.junit.Test
+import org.springframework.context.i18n.LocaleContextHolder
 
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
@@ -19,10 +21,26 @@ class DateConverterIntegrationTests   {
 
     def dateConverterService
 
+    public static final String ARABIC_LOCALE = "ar"
+    public static final String US_LOCALE = "en_US"
+
+    @After
+    public void tearDown() {
+        println "************** Locale values for different Scopes *************"
+        println "RequestContextAttributes => " + LocaleUtilities.getLocales()?.collect{Locale locale -> locale}
+        println "JVM Locale => " + Locale.default
+        println "Current Thread Locale => " + LocaleContextHolder.locale
+        println "Grails Web Request => " + GrailsWebRequest.lookup().getLocale()
+        println "i18n localizerService resolved locale => " + dateConverterService.localizerService(code: "default.calendar.gregorian.translation")
+        println "i18n MessageHelper.message resolved locale=> " + MessageHelper.message(code: "default.calendar.gregorian.translation")
+        println ("***************************************************")
+    }
+
 
     @Test
     void testInjection() {
         assertNotNull dateConverterService
+
     }
 
     @Test
@@ -96,7 +114,7 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testExtractDatePartsInEnglishLocale(){
-        LocaleUtilities.setLocale("en_US")
+        LocaleUtilities.setLocale(US_LOCALE)
         Map dateParts = dateConverterService.convertGregorianToDefaultCalendarAndExtractDateParts(new GregorianCalendar(2000,1,3).time)
         assertEquals dateParts["year"], "2000"
         assertEquals dateParts["month"], "02"
@@ -105,7 +123,7 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testExtractDatePartsInArabicLocale(){
-        LocaleUtilities.setLocale("ar")
+        LocaleUtilities.setLocale(ARABIC_LOCALE)
         Map dateParts = dateConverterService.convertGregorianToDefaultCalendarAndExtractDateParts(new GregorianCalendar(2005,1,3).time)
         assertEquals dateParts["year"], "1425"
         assertEquals dateParts["month"], "12"
@@ -114,7 +132,7 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testExtractDatePartsInFrenchLocale(){
-        LocaleUtilities.setLocale("fr_CA")
+        LocaleUtilities.setLocale(Locale.CANADA_FRENCH)
         Map dateParts = dateConverterService.convertGregorianToDefaultCalendarAndExtractDateParts(new GregorianCalendar(2000,1,3).time)
         assertEquals dateParts["year"], "2000"
         assertEquals dateParts["month"], "02"
@@ -129,7 +147,7 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testGetStartDateInGregorianCalendarFromEnglishLocale(){
-        LocaleUtilities.setLocale("en_US")
+        LocaleUtilities.setLocale(US_LOCALE)
         String gregorianStartDateForYear = formatDate(dateConverterService.getStartDateInGregorianCalendar(2010))
         assertEquals gregorianStartDateForYear, "2010/01/01"
 
@@ -139,7 +157,7 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testGetStartDateInGregorianCalendarFromArabicLocale(){
-        LocaleUtilities.setLocale("ar")
+        LocaleUtilities.setLocale(ARABIC_LOCALE)
         String gregorianStartDateForYear = formatDate(dateConverterService.getStartDateInGregorianCalendar(1433))
         assertEquals gregorianStartDateForYear, "2011/11/27"
 
@@ -149,7 +167,7 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testGetStartDateInGregorianCalendarFromFrenchLocale(){
-        LocaleUtilities.setLocale("fr_CA")
+        LocaleUtilities.setLocale(Locale.CANADA_FRENCH)
         String gregorianStartDateForYear = formatDate(dateConverterService.getStartDateInGregorianCalendar(2010))
         assertEquals gregorianStartDateForYear, "2010/01/01"
 
@@ -159,7 +177,7 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testGetEndDateInGregorianCalendarFromEnglishLocale(){
-        LocaleUtilities.setLocale("en_US")
+        LocaleUtilities.setLocale(US_LOCALE)
         String endDateFromYear = formatDate(dateConverterService.getEndDateInGregorianCalendar(2010))
         assertEquals endDateFromYear, "2010/12/31"
 
@@ -169,7 +187,7 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testGetEndDateInGregorianCalendarFromArabicLocale(){
-        LocaleUtilities.setLocale("ar")
+        LocaleUtilities.setLocale(ARABIC_LOCALE)
         String endDateFromYear = formatDate(dateConverterService.getEndDateInGregorianCalendar(1433))
         assertEquals endDateFromYear, "2012/11/14"
 
@@ -179,7 +197,7 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testGetEndDateInGregorianCalendarFromFrenchLocale(){
-        LocaleUtilities.setLocale("fr_CA")
+        LocaleUtilities.setLocale(Locale.CANADA_FRENCH)
         String endDateFromYear = formatDate(dateConverterService.getEndDateInGregorianCalendar(2010))
         assertEquals endDateFromYear, "2010/12/31"
 
@@ -189,14 +207,14 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testGetMonthNameFromCodeInEnglishLocale(){
-        LocaleUtilities.setLocale("en_US")
+        LocaleUtilities.setLocale(US_LOCALE)
         Map monthNamesWithCode = dateConverterService.getMonthNamesWithCode()
         assertEquals monthNamesWithCode.get(2), "March"
     }
 
     @Test
     void testGetMonthNameFromCodeInArabicLocale(){
-        LocaleUtilities.setLocale("ar")
+        LocaleUtilities.setLocale(ARABIC_LOCALE)
         Map monthNamesWithCode = dateConverterService.getMonthNamesWithCode()
         String uniCodeForThirdIslamicMonth = "\u0631"+"\u0628"+"\u064a"+"\u0639 "+"\u0627"+"\u0644"+"\u0623"+"\u0648"+"\u0644"
         assertEquals monthNamesWithCode.get(2), uniCodeForThirdIslamicMonth
@@ -204,15 +222,15 @@ class DateConverterIntegrationTests   {
 
     @Test
     void testGetGregorianMonthNameInArabicLocale(){
-        LocaleUtilities.setLocale("en_US")
-        Map monthNamesWithCode = dateConverterService.getMonthNamesWithCode("ar")
+        LocaleUtilities.setLocale(US_LOCALE)
+        Map monthNamesWithCode = dateConverterService.getMonthNamesWithCode(ARABIC_LOCALE)
         String uniCodeForFirstGregorianMonthInArabicLocale = "\u064a"+"\u0646"+"\u0627"+"\u064a"+"\u0631"
         assertEquals monthNamesWithCode.get(0), uniCodeForFirstGregorianMonthInArabicLocale
     }
 
     @Test
     void testGetMonthNameFromCodeInFrenchLocale(){
-        LocaleUtilities.setLocale("fr_CA")
+        LocaleUtilities.setLocale(Locale.CANADA_FRENCH)
         Map monthNamesWithCode = dateConverterService.getMonthNamesWithCode()
         String uniCodeForSecondMonthInFrench = "\u0066"+"\u00e9"+"\u0076"+"\u0072"+"\u0069"+"\u0065"+"\u0072"
         assertEquals monthNamesWithCode.get(1), uniCodeForSecondMonthInFrench
