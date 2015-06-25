db8f28c02cc150416fe78b30a26f0d206fc43d64
Shaliya Khwaja <shaliya.khwaja@ellucian.com>
2015-06-25 11:36:06 +0530

    HRU-5980 - Modified for Code Review comments- Added copyright and changed String values

--- test/integration/net/hedtech/banner/i18n/CurrencyFormatterIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/i18n/CurrencyFormatterIntegrationTests.groovy
@@ -1,9 +1,11 @@
+/*******************************************************************************
+ Copyright 2009-2015 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
 package net.hedtech.banner.i18n
 
 import net.hedtech.banner.exceptions.CurrencyNotFoundException
 import org.junit.Test
 
-import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertNotNull
 import static org.junit.Assert.assertTrue
@@ -13,6 +15,34 @@ import static org.junit.Assert.assertTrue
  */
 class CurrencyFormatterIntegrationTests {
 def currencyFormatService
+    private final String POSITIVE_TEST_VALUE = "1234567890.239"
+    private final String NEGATIVE_TEST_VALUE ="-1234567890.239"
+    private final String USD ="USD"
+    private final String AUD ="AUD"
+    private final String CAD ="CAD"
+    private final String GBP ="GBP"
+    private final String PTE ="PTE"
+    private final String EUR ="EUR"
+    private final String SAR ="SAR"
+    private final String USD_POS_TEST_VALUE = "\$1,234,567,890.24"
+    private final String USD_NEG_TEST_VALUE = "(\$1,234,567,890.24)"
+    private final String AUD_POS_TEST_VALUE = "AU\$1,234,567,890.24"
+    private final String AUD_NEG_TEST_VALUE = "(AU\$1,234,567,890.24)"
+    private final String CAD_POS_TEST_VALUE = "CA\$1,234,567,890.24"
+    private final String CAD_NEG_TEST_VALUE = "(CA\$1,234,567,890.24)"
+    private final String GBP_POS_TEST_VALUE = "£1,234,567,890.24"
+    private final String GBP_NEG_TEST_VALUE = "(£1,234,567,890.24)"
+    private final String PTE_POS_TEST_VALUE = "PTE1,234,567,890.24"
+    private final String PTE_NEG_TEST_VALUE = "(PTE1,234,567,890.24)"
+    private final String EUR_POS_TEST_VALUE = "€1,234,567,890.24"
+    private final String EUR_NEG_TEST_VALUE = "(€1,234,567,890.24)"
+    private final String SAR_POS_TEST_VALUE = "SAR1,234,567,890.24"
+    private final String SAR_NEG_TEST_VALUE = "(SAR1,234,567,890.24)"
+
+    private final String INVALID_CODE ="XYZ"
+    private final String INVALID_CODE_MESSAGE = "Invalid currency code"
+
+
 
     @Test
     void testInjection() {
@@ -22,78 +52,78 @@ def currencyFormatService
 
     @Test
     void testUSDPositiveCurrencyFormatter() {
-        assertEquals currencyFormatService.format("USD",new BigDecimal(1234567890.239)),"\$1,234,567,890.24"
+        assertEquals currencyFormatService.format(USD,new BigDecimal(POSITIVE_TEST_VALUE)),USD_POS_TEST_VALUE
         }
 
     @Test
     void testUSDNegativeCurrencyFormatter() {
-        assertEquals currencyFormatService.format("USD",new BigDecimal(-1234567890.239)),"(\$1,234,567,890.24)"
+        assertEquals currencyFormatService.format(USD,new BigDecimal(NEGATIVE_TEST_VALUE)),USD_NEG_TEST_VALUE
     }
     @Test
     void testAUDPositiveCurrencyFormatter() {
-        assertEquals currencyFormatService.format("AUD",new BigDecimal(1234567890.239)),"AU\$1,234,567,890.24"
+        assertEquals currencyFormatService.format(AUD,new BigDecimal(POSITIVE_TEST_VALUE)),AUD_POS_TEST_VALUE
     }
 
     @Test
     void testAUDNegativeCurrencyFormatter() {
-        assertEquals currencyFormatService.format("AUD",new BigDecimal(-1234567890.239)),"(AU\$1,234,567,890.24)"
+        assertEquals currencyFormatService.format(AUD,new BigDecimal(NEGATIVE_TEST_VALUE)),AUD_NEG_TEST_VALUE
     }
     @Test
     void testCADPositiveCurrencyFormatter() {
-        assertEquals currencyFormatService.format("CAD",new BigDecimal(1234567890.239)),"CA\$1,234,567,890.24"
+        assertEquals currencyFormatService.format(CAD,new BigDecimal(POSITIVE_TEST_VALUE)),CAD_POS_TEST_VALUE
     }
 
     @Test
     void testCADNegativeCurrencyFormatter() {
-        assertEquals currencyFormatService.format("CAD",new BigDecimal(-1234567890.239)),"(CA\$1,234,567,890.24)"
+        assertEquals currencyFormatService.format(CAD,new BigDecimal(NEGATIVE_TEST_VALUE)),CAD_NEG_TEST_VALUE
     }
     @Test
     void testGBPPositiveCurrencyFormatter() {
-        assertEquals currencyFormatService.format("GBP",new BigDecimal(1234567890.239)),"£1,234,567,890.24"
+        assertEquals currencyFormatService.format(GBP,new BigDecimal(POSITIVE_TEST_VALUE)),GBP_POS_TEST_VALUE
     }
 
     @Test
     void testGBPNegativeCurrencyFormatter() {
-        assertEquals currencyFormatService.format("GBP",new BigDecimal(-1234567890.239)),"(£1,234,567,890.24)"
+        assertEquals currencyFormatService.format(GBP,new BigDecimal(NEGATIVE_TEST_VALUE)),GBP_NEG_TEST_VALUE
     }
     @Test
     void testPTEPositiveCurrencyFormatter() {
-        assertEquals currencyFormatService.format("PTE",new BigDecimal(1234567890.239)),"PTE1,234,567,890.24"
+        assertEquals currencyFormatService.format(PTE,new BigDecimal(POSITIVE_TEST_VALUE)),PTE_POS_TEST_VALUE
     }
 
     @Test
     void testPTENegativeCurrencyFormatter() {
-        assertEquals currencyFormatService.format("PTE",new BigDecimal(-1234567890.239)),"(PTE1,234,567,890.24)"
+        assertEquals currencyFormatService.format(PTE,new BigDecimal(NEGATIVE_TEST_VALUE)),PTE_NEG_TEST_VALUE
     }
     @Test
     void testEURPositiveCurrencyFormatter() {
-        assertEquals currencyFormatService.format("EUR",new BigDecimal(1234567890.239)),"€1,234,567,890.24"
+        assertEquals currencyFormatService.format(EUR,new BigDecimal(POSITIVE_TEST_VALUE)),EUR_POS_TEST_VALUE
     }
 
     @Test
     void testEURNegativeCurrencyFormatter() {
-        assertEquals currencyFormatService.format("EUR",new BigDecimal(-1234567890.239)),"(€1,234,567,890.24)"
+        assertEquals currencyFormatService.format(EUR,new BigDecimal(NEGATIVE_TEST_VALUE)),EUR_NEG_TEST_VALUE
     }
 
     @Test
     void testSARPositiveCurrencyFormatter() {
-        assertEquals currencyFormatService.format("SAR",new BigDecimal(1234567890.239)),"SAR1,234,567,890.24"
+        assertEquals currencyFormatService.format(SAR,new BigDecimal(POSITIVE_TEST_VALUE)),SAR_POS_TEST_VALUE
     }
 
     @Test
     void testSARNegativeCurrencyFormatter() {
-        assertEquals currencyFormatService.format("SAR",new BigDecimal(-1234567890.239)),"(SAR1,234,567,890.24)"
+        assertEquals currencyFormatService.format(SAR,new BigDecimal(NEGATIVE_TEST_VALUE)),SAR_NEG_TEST_VALUE
     }
     @Test
     void testInvalidCurrencyFormatter() {
         Exception exception = null
 
         try {
-            currencyFormatService.format("XYZ",new BigDecimal(1234567890.239))
+            currencyFormatService.format(INVALID_CODE,new BigDecimal(POSITIVE_TEST_VALUE))
         } catch (CurrencyNotFoundException e) {
             exception = e;
         }
-        assertTrue(exception.getMessage().contains("Invalid currency code"))
+        assertTrue(exception.getMessage().contains(INVALID_CODE_MESSAGE))
     }
 
 }
