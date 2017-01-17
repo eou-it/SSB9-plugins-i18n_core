3d35bc203f0437ea236adce37f28b82f741a1164
gurunath_dk <gurunat.dk@ellucian.com>
2017-01-17 14:28:01 +0530

    Root(usEN) message type reading data

--- grails-app/services/net/hedtech/banner/i18n/TextManagerService.groovy
+++ grails-app/services/net/hedtech/banner/i18n/TextManagerService.groovy
@@ -174,8 +174,8 @@ class TextManagerService {
             msg = cacheMsg[key]?cacheMsg[key][locale]:null
         } else {
             def tmLocale = locale?.toString().replace('_','')
-            tmLocale = (tmLocale =="enUS")? ROOT_LOCALE_TM:tmLocale
             def tmProject = tranManProject()
+            tmLocale = (tmLocale =="enUS")? ROOT_LOCALE_TM:tmLocale
             def since = new java.sql.Timestamp(localeLoaded[locale]?localeLoaded[locale].getTime():0) // 0 is like beginning of time
             def params = [locale: tmLocale, pc: tmProject, now: new java.sql.Timestamp(t0.getTime()), since: since]
             def tmdbif = new Dbif(connectString, null) // get a standard connection
