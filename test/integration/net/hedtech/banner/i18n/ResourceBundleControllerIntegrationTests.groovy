ade8be45d68df70ed13a64a990c62542675188c3
Shaliya Khwaja <shaliya.khwaja@ellucian.com>
2017-06-29 13:15:22 +0530

    EXTZ-1820- Removed commented code

--- test/integration/net/hedtech/banner/i18n/ResourceBundleControllerIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/i18n/ResourceBundleControllerIntegrationTests.groovy
@@ -37,35 +37,4 @@ class ResourceBundleControllerIntegrationTests {
         resourceBundleController.save()
         assert 200,resourceBundleController.response.status
     }
-
-//
-//
-//
-//    void "Test for fetching list of values"(){
-//        when:
-//        resourcBundle.list();
-//        then:
-//        resourcBundle.response.text !=null
-//    }
-//
-//    void "Test for fetching values based on id and type"(){
-//        given:
-//        resourcBundle.params.id =40
-//        resourcBundle.params.name="PLUGINS/CSV/MESSAGES"
-//        resourcBundle.params.locale= "en_US"
-//        when:
-//        resourcBundle.show();
-//        then:
-//        resourcBundle.response.text !=null
-//    }
-//
-//    void "Test for saving values"() {
-//        given:
-//        def data = ['id': '40', 'name': 'PLUGINS/CSV/MESSAGES', 'locale': 'en_US']
-//        resourcBundle.request.JSON = data
-//        when:
-//        resourcBundle.save();
-//        then:
-//        resourcBundle.response.text != null
-//    }
 }
