d44035b8324dca2c38b2001f211648a6480b65b3
Avinash Hinduja <avinash.hinduja@ellucian.com>
2018-06-11 15:22:58 +0530

    adding dependicies and deleting build folder

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '0.1')
-        classNode.putNodeMetaData('projectName', 'i18n_core.git')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
