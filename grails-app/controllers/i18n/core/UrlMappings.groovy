8ce139542e6b95866a4d26fc85aafa348f7ebd76
Ankur Jain <ankur.jain@ellucian.com>
2018-12-28 12:08:04 +0530

    Removed extra UrlMappings.groovy class and moved relevant content to main UrlMappings.groovy class HRU-8280

--- grails-app/controllers/i18n/core/UrlMappings.groovy
+++ grails-app/controllers/i18n/core/UrlMappings.groovy
@@ -2,6 +2,23 @@ package i18n.core
 
 class UrlMappings {
 
-    static mappings = {}
+    static mappings = {
+//        "/dateConverter/$date/$fromULocale?/$toULocale?/$fromDateFormat?/$toDateFormat?/$adjustDays?"(controller: "dateConverter")
+//
+//        //TM interface
+//        "/admin/i18n/$id"(controller:'resourceBundle') {
+//            action = [GET: "show", PUT: "save",
+//                      DELETE: "delete"]
+//            parseRequest = false
+//            constraints {
+//                // to constrain the id to numeric, uncomment the following:
+//                // id matches: /\d+/
+//            }
+//        }
+//        "/admin/i18n/"(controller:'resourceBundle') {
+//            action = [GET: "list", POST: "save"]
+//            parseRequest = false
+//        }
+    }
 
 }
